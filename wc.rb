a201059fb9e507763fb6911447f7bb914980c6fc
YutoUrushima <yuto259233@gmail.com>
2022-12-01 22:57:17 +0900

    Add prettier

--- wc.rb
+++ wc.rb
@@ -1,37 +1,34 @@
 #!/usr/bin/env ruby
-require 'optparse'
+require "optparse"
 
 MESSAGE = "I don't know any countries like that, so the world is bigger than I thought."
 WIN_MESSAGE = "Japan win!"
 LOSE_MESSAGE = "Japan lose..."
 
 class WorldCup2022
-
-    def initialize
-        @games = {}
-        OptionParser.new do |opt|
-            opt.on("-g", "--germany", "Show the winners and losers of the match between Japan and Germany") {|val| @games[:result] = WIN_MESSAGE }
-            opt.on("-c", "--costarica", "Show the winners and losers of the match between Japan and Costa Rica") {|val| @game[:result] = LOSE_MESSAGE }
-            opt.parse(ARGV)
-        rescue OptionParser::InvalidOption => error
-            self.message
-        end
+  def initialize
+    @games = {}
+    OptionParser.new do |opt|
+      opt.on("-g", "--germany", "Show the winners and losers of the match between Japan and Germany") { |val| @games[:result] = WIN_MESSAGE }
+      opt.on("-c", "--costarica", "Show the winners and losers of the match between Japan and Costa Rica") { |val| @game[:result] = LOSE_MESSAGE }
+      opt.parse(ARGV)
+    rescue OptionParser::InvalidOption => error
+      self.message
     end
+  end
 
-    def message
-        puts MESSAGE
-    end
+  def message
+    puts MESSAGE
+  end
 
-    def specify?
-        return !!@games[:result]
-    end
+  def specify?
+    return !!@games[:result]
+  end
 
-    def get_the_result
-        puts @games[:result]
-    end
+  def get_the_result
+    puts @games[:result]
+  end
 end
 
 worldcup2022 = WorldCup2022.new
-if worldcup2022.specify?
-    worldcup2022.get_the_result
-end
+worldcup2022.get_the_result if worldcup2022.specify?
