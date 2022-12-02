dc44a592dee9de6394bf1e7d6c3dc67afc8f5f36
YutoUrushima <yuto259233@gmail.com>
2022-12-02 23:00:11 +0900

    Fix validate

--- wc.rb
+++ wc.rb
@@ -1,32 +1,49 @@
 #!/usr/bin/env ruby
 require "optparse"
 
-MESSAGE = "I don't know any countries like that, so the world is bigger than I thought."
+MESSAGE =
+  "I don't know any countries like that, so the world is bigger than I thought."
 WIN_MESSAGE = "Japan win!"
 LOSE_MESSAGE = "Japan lose..."
 
 class WorldCup2022
   def initialize
-    @games = {}
+    @result = nil
     OptionParser.new do |opt|
-      opt.on("-g", "--germany", "Show the winners and losers of the match between Japan and Germany") { |val| @games[:result] = WIN_MESSAGE }
-      opt.on("-c", "--costarica", "Show the winners and losers of the match between Japan and Costa Rica") { |val| @games[:result] = LOSE_MESSAGE }
+      opt.on(
+        "-g",
+        "--germany",
+        "Show the winners and losers of the match between Japan and Germany"
+      ) { |val| @result = WIN_MESSAGE }
+      opt.on(
+        "-c",
+        "--costarica",
+        "Show the winners and losers of the match between Japan and Costa Rica"
+      ) { |val| @result = LOSE_MESSAGE }
+      raise "Input correctly" unless self.is_valid?
       opt.parse(ARGV)
     rescue OptionParser::InvalidOption => error
-      puts self.message
+      puts MESSAGE
+    rescue => error
+      puts error
     end
   end
 
-  def message
-    return MESSAGE
-  end
-
   def specify?
-    return !!@games[:result]
+    return !!@result
   end
 
   def get_the_result
-    return @games[:result]
+    return @result
+  end
+
+  def is_valid?
+    return false if ARGV.size >= 2
+    option_word = ARGV[0]
+    if "-" == option_word[0] && "-" != option_word[1]
+      return option_word.size <= 2
+    end
+    return true
   end
 end
 
