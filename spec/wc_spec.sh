5798fd8eaa18fb4e81f5ae37ad8234b41a89f65d
YutoUrushima <yuto259233@gmail.com>
2022-12-04 14:50:05 +0900

    Fix deatils & Write some test

--- spec/wc_spec.sh
+++ spec/wc_spec.sh
@@ -1,3 +1,15 @@
 Describe "wc.rb" do
-  // here
-end
+
+  echo "TEST BEGIN"
+
+  cat ./setting.json | jq '.constants.message'
+  cat ./setting.json | jq '.constants.win'
+  cat ./setting.json | jq '.constants.lose'
+  cat ./setting.json | jq '.constants.error'
+
+  It 'ex'
+    When run ./wc
+    The output should equal "Input correctly"
+  End
+End
+
