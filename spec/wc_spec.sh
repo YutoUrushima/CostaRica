97e721117d3a4cddc39fdd5bd9df8b9eb78ffe66
YutoUrushima <yuto259233@gmail.com>
2022-12-07 21:26:41 +0900

    Success to exec test

--- spec/wc_spec.sh
+++ spec/wc_spec.sh
@@ -2,14 +2,19 @@ Describe "wc.rb" do
 
   echo "TEST BEGIN"
 
-  cat ./setting.json | jq '.constants.message'
-  cat ./setting.json | jq '.constants.win'
-  cat ./setting.json | jq '.constants.lose'
-  cat ./setting.json | jq '.constants.error'
+  # cat ./setting.json | jq '.constants.message'
+  # cat ./setting.json | jq '.constants.win'
+  # cat ./setting.json | jq '.constants.lose'
+  # cat ./setting.json | jq '.constants.error'
 
-  It 'ex'
+  It 'should error message when call with no option'
     When run ./wc
     The output should equal "Input correctly"
+  End
+
+  It 'should win message when call with germany option'
+    When run ./wc -g
+    The output should equal "Japan win!"
   End
 End
 
